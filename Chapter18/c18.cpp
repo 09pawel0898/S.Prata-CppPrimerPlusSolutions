7fc7e30c3a04b732987d606da67adaaebc78788f
09pawel0898 <38491170+09pawel0898@users.noreply.github.com>
2021-07-27 13:46:14 +0200

    Update c18.cpp

--- Chapter18/c18.cpp
+++ Chapter18/c18.cpp
@@ -1,7 +1,7 @@
 ﻿#include "ex18_1.h"
 #include "ex18_2.h"
 #include "ex18_3.h"
-#include "ex18_4.h"
+
 
 int main(int argc, char **argv)
 {
@@ -16,7 +16,6 @@ int main(int argc, char **argv)
         case 1: ex1();  break;
         case 2: ex2();  break;
         case 3: ex3();  break;
-        case 4: ex4();  break;
     default:
         std::cout << "Theres no such excersise";
         break;
